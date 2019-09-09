cc190b0862ddf69a5c9755e82aa0204c6d9a54ae
Nikola Milosevic <32174877+deox123@users.noreply.github.com>
2019-09-09 18:38:36 +0200

    Update dashboardContext.js

--- client/src/context/dashboardContext.js
+++ client/src/context/dashboardContext.js
@@ -4,7 +4,14 @@ import dashboardReducer from "./dashboardReducer";
 const initialState = {
   startFetching: false,
   status: "",
+  // initial state treba da bude prazan niz za companies, pogotovo ako iteriras po njemu u aplikaciji
+  // brobaj ovo u conzoli
+  // const asd = null;
+  // asd.lenght
+  // asd.map(a => console.log(a))
   companies: null,
+  
+  // proveri i za ostale elemente, recimo ako imas negde officeInfo.nesto ce pucati ako je officeInfo: null
   dailyInfo: null,
   officeInfo: null,
   error: ""
