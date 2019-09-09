5309d0dfca8d8e3d346f265ad1b5680cf4021411
Tamara Tibold <tamara.milosavljevic89@gmail.com>
2019-09-09 22:05:30 +0200

    bar_chart_created

--- client/src/components/Dashboard/barChart/BarChart.js
+++ client/src/components/Dashboard/barChart/BarChart.js
@@ -1,90 +1,121 @@
-import React, { useEffect, useCallback, useState } from "react";
-import "./bar_chart.scss";
-import * as d3 from "d3";
+import React,{useCallback,useState,useEffect} from 'react';
+import * as d3 from 'd3'
+const companies = [
+  {
+    name: "google",
+    visitors: Math.floor(Math.random() * (20 - 10 + 1) + 10)
+  },
+  {
+    name: "amazon",
+    visitors: Math.floor(Math.random() * (20 - 10 + 1) + 10)
+  },
+  {
+    name: "elsevier",
+    visitors: Math.floor(Math.random() * (20 - 10 + 1) + 10)
+  },
+  {
+    name: "tomtom",
+    visitors: Math.floor(Math.random() * (20 - 10 + 1) + 10)
+  },
+  {
+    name: "uber",
+    visitors: Math.floor(Math.random() * (20 - 10 + 1) + 10)
+  },
+  {
+    name: "facebook",
+    visitors: Math.floor(Math.random() * (20 - 10 + 1) + 10)
+  }
+];
 
-const BarChart = ({ companies }) => {
+const BarChart = () => {
   let [sumOfVisitors, setSumOfVisitors] = useState(0);
   console.log(companies);
 
   const drawChart = useCallback(() => {
     const scaleStart = d3.min(companies, c => c.visitors);
     const scaleEnd = d3.max(companies, c => c.visitors);
-       const myColor = d3
+    const myColor = d3
       .scaleOrdinal()
       .domain([scaleStart, scaleEnd])
       .range(["#F8C675", "rgb(221, 44, 74)"]);
-      var margin = { top: 10, right: 20, bottom: 60, left: 30 };
-      var width = 400 - margin.left - margin.right;
-      var height = 565 - margin.top - margin.bottom;
-      
-      var svg = d3.select('.bar_chart')
-        .append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
-          .call(responsivefy)
-        .append('g')
-          .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-      
-      
-      var yScale = d3.scaleLinear()
-        .domain([scaleStart,scaleEnd])
-        .range([height, 0]);
-     
-    var xScale = d3
-      .scaleBand()
+
+
+    var margin = { top: 10, right: 20, bottom: 60, left: 30 };
+    var width = 400 - margin.left - margin.right;
+    var height = 565 - margin.top - margin.bottom;
+
+    var svg = d3
+      .select(".bar_chart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .call(responsivefy)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+    var yScale = d3
+      .scaleLinear()
+      .domain([0, scaleEnd * 1.2])
+      .range([height, 0]);
+    var yAxis = d3.axisLeft(yScale);
+    svg.call(yAxis);
+
+    var xScale = d3.scaleBand()
       .padding(0.2)
       .domain(companies.map(d => d.name))
       .range([0, width]);
-      
-    var xAxis = d3
-      .axisBottom(xScale)
+
+    var xAxis = d3.axisBottom(xScale)
       .ticks(5)
       .tickSize(10)
       .tickPadding(5);
     svg
       .append('g')
-      .attr("transform", `translate(0, ${height})`)
+      .attr('transform', `translate(0, ${height})`)
       .call(xAxis)
       .selectAll('text')
-      .style("text-anchor", "end")
-      .attr("transform", "rotate(-45)");
+      .style('text-anchor', 'end')
+      .attr('transform', 'rotate(-45)');
 
-      
-      svg.selectAll('rect')
-        .data(companies)
-        .enter()
-        .append('rect')
-        .attr('x', d => xScale(d.name))
-        .attr('y', d => yScale(d.visitors))
-        .attr('width', d => xScale.bandwidth())
-        .attr('height', d => height - yScale(d.visitors));
-      
-      function responsivefy(svg) {
-        // get container + svg aspect ratio
-        var container = d3.select(svg.node().parentNode),
-            width = parseInt(svg.style("width")),
-            height = parseInt(svg.style("height")),
-            aspect = width / height;
-      
-        // add viewBox and preserveAspectRatio properties,
-        // and call resize so that svg resizes on inital page load
-        svg.attr("viewBox", "0 0 " + width + " " + height)
-            .attr("preserveAspectRatio", "xMinYMid")
-            .call(resize);
-      
-        // to register multiple listeners for same event type,
-        // you need to add namespace, i.e., 'click.foo'
-        // necessary if you call invoke this function for multiple svgs
-        // api docs: https://github.com/mbostock/d3/wiki/Selections#on
-        d3.select(window).on("resize." + container.attr("id"), resize);
-      
-        // get width of container and resize svg to fit it
-        function resize() {
-            var targetWidth = parseInt(container.style("width"));
-            svg.attr("width", targetWidth);
-            svg.attr("height", Math.round(targetWidth / aspect));
-        }
+    svg.selectAll('rect')
+      .data(companies)
+      .enter()
+      .append('rect')
+      .attr('x', d => xScale(d.name))
+      .attr('y', d => yScale(d.visitors))
+      .attr("width", d => xScale.bandwidth())
+      .attr("height", d => height - yScale(d.visitors))
+      .style("fill", d => myColor(d.visitors));
+
+    function responsivefy(svg) {
+      // get container + svg aspect ratio
+      var container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+
+      // add viewBox and preserveAspectRatio properties,
+      // and call resize so that svg resizes on inital page load
+      svg
+        .attr("viewBox", "0 0 " + width + " " + height)
+        .attr("preserveAspectRatio", "xMinYMid")
+        .call(resize);
+
+      // to register multiple listeners for same event type,
+      // you need to add namespace, i.e., 'click.foo'
+      // necessary if you call invoke this function for multiple svgs
+      // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+      d3.select(window).on("resize." + container.attr("id"), resize);
+
+      // get width of container and resize svg to fit it
+      function resize() {
+        var targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
       }
+    }
+
+
   }, [companies]);
 
   useEffect(() => {
@@ -103,4 +134,5 @@ const BarChart = ({ companies }) => {
     </div>
   );
 };
+
 export default BarChart;
